53b416726e8fa84815e1d3f589dc3a1dc0cf2da3
giogaf <giogaf@gmail.com>
2016-06-16 11:43:18 -0500

    title-page helper for  show project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def titulo(*parts)
+		unless parts.empty?
+			content_for :titulo do
+				(parts <<'Tikeg').join(' - ')
+			end
+		end
+	end
 end
--- spec/features/crear_proyecto_spec.rb
+++ spec/features/crear_proyecto_spec.rb
@@ -7,5 +7,11 @@ RSpec.feature "Usuario crea nuevo proyecto" do
 		fill_in 'Nombre',with: 'proyecto numero 123'
 		fill_in 'Descripcion',with: 'aprendiendo testing con RSpec y capibara'
 		click_button 'Crear proyecto'
+
+		project = Project.find_by(nombre:'proyecto numero 123')
+		expect(page.current_url).to eq project_url(project)
+
+		titulo = 'proyecto numero 123 - Tikeg'
+		expect(page).to have_title titulo
 	end
 end
