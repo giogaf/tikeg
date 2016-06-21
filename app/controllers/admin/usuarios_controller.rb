7dcfd93eaa3648c6219a86863cd759a7520401aa
giogaf <giogaf@gmail.com>
2016-06-21 08:32:11 -0500

    admin backend: new users

--- app/controllers/admin/usuarios_controller.rb
+++ app/controllers/admin/usuarios_controller.rb
@@ -1,5 +1,37 @@
 class Admin::UsuariosController < Admin::EscritorioController#ApplicationController
+    before_action :set_user,only: [:show,:edit,:update,:destroy]
+    before_action :params
   def index
   	@users =  User.order(:email)
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def show
+  	
+  end
+
+  def create
+  	@user  =  User.new(params_user)
+  	if @user.save
+  		flash[:notice] = "User has been created."
+  		redirect_to admin_usuarios_path #@user
+  	else
+  		flash[alert] = "User has not been created."
+  		render :new
+  	end
+  end
+
+  private
+
+  def set_user
+  	@user = User.find(params[:id])
+  end
+
+  def params_user
+  	params.require(:user).permit(:email,:password)
+  end
+
 end
